947cd77cb7b2b0d2dbf5bc66f5cb20c838b5f425
unknown <snishanth512@outlook.com>
2019-05-30 12:58:00 +0530

    code update

---
+++ index.js
@@ -0,0 +1,30 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var demoMsg = require('./demo.json')
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.on('browserMessage', function (msg) {
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(3000, function () {
+  console.log('listening on *:3000');
+});
+
+var i = 0;
+
+function periodicMessageSend() {
+  var message;
+  i++;
+  message = demoMsg;
+  message["messageId"] = i;
+  io.emit('browserMessage', JSON.stringify(message));
+}
+
+setInterval(periodicMessageSend, 3000);
